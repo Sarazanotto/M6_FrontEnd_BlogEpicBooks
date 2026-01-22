6a8a06ddf253cd627734a1ac0532cef07cd80718
sara zanotto <saraazanotto@gmail.com>
2026-01-22 20:31:45 +0100

    a

--- Frontend_epicbooks/src/App.jsx
+++ Frontend_epicbooks/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import ModifyUser from "./pages/ModifyUser"
 import DetailBook from "./pages/DetailBook"
@@ -9,7 +9,7 @@ function App() {
   return(
     <BrowserRouter>
     <Routes>
-      <Route index path='/' element={<LoginPage/>}/>
+      <Route index path='/' element={<Login/>}/>
       <Route  path='/homepage' element={<Homepage/>}/>
       <Route  path='/modifyUser' element={<ModifyUser/>}/>
       <Route  path='/loginPage' element={<LoginPage/>}/>
